18ddfb564c3a63dad166900a26fcb59ffd16e9a7
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-03-09 01:41:18 +0700

    add statistic card on dashboard

---
+++ src/components/UserCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  type: String,
+})
+</script>
+
+<template>
+  <div class="odd:bg-scl-purple even:bg-scl-yellow min-w-[130px] flex-1 rounded-2xl p-4">
+    <div class="flex items-center justify-between">
+      <span class="rounded-full bg-white px-2 py-1 text-xs text-green-600">2025/01</span>
+      <img src="/icon/more.png" alt="" class="size-5" />
+    </div>
+    <h1 class="my-4 text-2xl font-semibold">1,234</h1>
+    <h2 class="text-sm font-medium text-gray-500 capitalize">{{ type }}s</h2>
+  </div>
+</template>
--- src/pages/Dashboard/AdminPage.vue
+++ src/pages/Dashboard/AdminPage.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import UserCard from '@/components/UserCard.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 </script>
 
 <template>
   <DashboardLayout>
-    <div>AdminPage</div>
+    <div class="flex flex-col gap-4 p-4 md:flex-row">
+      <!-- left -->
+      <div class="w-full lg:w-2/3">
+        <div class="flex flex-wrap justify-between gap-4">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      <!-- right -->
+      <div class="w-full lg:w-1/3">right content</div>
+    </div>
   </DashboardLayout>
 </template>
