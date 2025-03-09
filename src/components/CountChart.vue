eb76838954edebc74ea9af511c3bb3b8769506d4
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-03-09 16:21:30 +0700

    add total student chart

---
+++ src/components/CountChart.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref } from 'vue'
+
+const series = ref([55, 45])
+const chartOptions = ref({
+  chart: {
+    type: 'radialBar',
+    parentHeightOffset: 100,
+  },
+  fill: {
+    colors: ['#c3ebfa', '#fae27c'],
+  },
+  plotOptions: {
+    radialBar: {
+      inverseOrder: false,
+      startAngle: 0,
+      endAngle: 360,
+      track: {
+        show: true,
+        startAngle: undefined,
+        endAngle: undefined,
+        background: '#f2f2f2',
+        strokeWidth: '97%',
+        opacity: 1,
+        margin: 3,
+        dropShadow: {
+          enabled: false,
+          top: 0,
+          left: 0,
+          blur: 3,
+          opacity: 0.5,
+        },
+      },
+      dataLabels: {
+        name: {
+          fontSize: '16px',
+        },
+        value: {
+          fontSize: '16px',
+        },
+      },
+    },
+  },
+  labels: ['Boys', 'Girls'],
+})
+</script>
+
+<template>
+  <div class="w-full rounded-2xl bg-white p-4">
+    <!-- title -->
+    <div class="flex items-center justify-between">
+      <h1 class="text-lg font-semibold">Students</h1>
+      <img src="/icon/moreDark.png" alt="more" class="size-5" />
+    </div>
+    <!-- chart -->
+    <div id="chart" class="w-full">
+      <apexchart width="100%" type="radialBar" :options="chartOptions" :series="series"></apexchart>
+    </div>
+    <!-- bottom -->
+    <div class="flex justify-center gap-16">
+      <div class="flex flex-col gap-1">
+        <div class="bg-scl-sky h-5 w-5 rounded-full" />
+        <h1 class="font-bold">1,234</h1>
+        <h2 class="text-xs text-gray-300">Boys (55%)</h2>
+      </div>
+      <div class="flex flex-col gap-1">
+        <div class="bg-scl-yellow h-5 w-5 rounded-full" />
+        <h1 class="font-bold">1,234</h1>
+        <h2 class="text-xs text-gray-300">Girls (45%)</h2>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/Dashboard/AdminPage.vue
+++ src/pages/Dashboard/AdminPage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import CountChart from '@/components/CountChart.vue'
 import UserCard from '@/components/UserCard.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 </script>
@@ -7,12 +8,20 @@ import DashboardLayout from '@/layouts/DashboardLayout.vue'
   <DashboardLayout>
     <div class="flex flex-col gap-4 p-4 md:flex-row">
       <!-- left -->
-      <div class="w-full lg:w-2/3">
+      <div class="flex w-full flex-col gap-8 lg:w-2/3">
         <div class="flex flex-wrap justify-between gap-4">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
+        </div>
+        <!-- middle chart -->
+        <div class="flex flex-col gap-4 lg:flex-row">
+          <!-- count chart -->
+          <div class="w-full lg:w-1/3">
+            <CountChart />
+          </div>
+          <div class="w-full lg:w-2/3"></div>
         </div>
       </div>
       <!-- right -->
