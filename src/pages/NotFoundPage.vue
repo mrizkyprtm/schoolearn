017be18c141620d11bba8ce5d23cde48f1cabec0
mrizkyprtm <m.rizkypratama319@gmail.com>
2025-03-10 16:59:51 +0700

    styling not found page

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Page Not Found</div>
+  <div class="bg-scl-purpleLight flex min-h-screen min-w-screen items-center justify-center">
+    <div class="">
+      <h1 class="text-2xl font-semibold">Oops!</h1>
+      <p class="text-4xl font-bold">Page not found.</p>
+    </div>
+  </div>
 </template>
